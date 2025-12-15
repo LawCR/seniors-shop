600e33b5de553e95df65704511520eaa88506e29
Alvaro Calderon <alvacalderon99@gmail.com>
2025-12-15 16:02:12 -0500

    hero imagen 2 cambiado

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,7 +23,7 @@ export function Navbar() {
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
-            <Logo size="md" />
+            <Logo size="sm" />
             <span className="text-lg font-bold">Manos de Vida</span>
           </Link>
 
